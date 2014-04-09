05c7ab7296929baea552c3dc84dd34e653cbf85d
Cam Spiers <camspiers@gmail.com>
2014-04-09 22:00:35 +1200

    Docs and code coverage

---
+++ api/1.1.3/tree.js
@@ -0,0 +1,10 @@
+var tree = [["[Global Namespace]",,"",
+                [["CacheIncludeContext","CacheIncludeContext.html","",
+                [        ]],["CacheIncludeContextInterface","CacheIncludeContextInterface.html","",
+                [        ]],["CacheIncludeController","CacheIncludeController.html"," < Controller",
+                [        ]],["CacheIncludeExtension","CacheIncludeExtension.html"," < Extension",
+                [        ]],["CacheIncludeQueueItem","CacheIncludeQueueItem.html"," < DataObject",
+                [        ]],["CacheIncludeSiteTreeDecorator","CacheIncludeSiteTreeDecorator.html"," < DataObjectDecorator",
+                [        ]],["ClearTemplates","ClearTemplates.html"," < ScheduledTask",
+                [        ]]        ]]        ]
+
